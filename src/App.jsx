3f8426797b7e86ed4214be5668114a3d68f4154b
CIAC <CIAC@CIAC2-005.academic.mixcoac.upmx.mx>
2025-10-09 13:09:56 -0600

    G

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Layout from "./components/layout";
+function App() {
+ 
+
+  return (
+    <>
+    <Layout></Layout>
+    </>
+  )
+}
+
+export default App
